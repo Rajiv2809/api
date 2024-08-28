09ed063f1fb90092bc790f5f4f4df4cc6b385aa1
Alchemist. <121161360+Rajiv2809@users.noreply.github.com>
2024-08-28 19:35:56 +0700

    Initial commit

---
+++ app/Http/Controllers/CaptchaController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
+
+class CaptchaController extends Controller
+{
+<<<<<<< HEAD
+    private function generateRandomString($length = 6)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
+    }
+    public function captcha()
+    {
+        // Ambil UUID
+        $uuid = Str::uuid()->toString();
+
+        // Bagian pertama UUID sebagai token
+        $token = explode('-', $uuid)[0];
+
+        // Bagian terakhir UUID sebagai CAPTCHA code, diambil 6 karakter
+        $captchaCode = substr(explode('-', $uuid)[4], 0, 6);
+
+        // Simpan CAPTCHA code dalam cache dengan token sebagai kunci
+        Cache::put($token, $captchaCode, 300); 
+=======
+    // private function generateRandomString($length = 6)
+    // {
+    //     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    //     $randomString = '';
+    //     for ($i = 0; $i < $length; $i++) {
+    //         $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    //     }
+    //     return $randomString;
+    // }
+    public function requestToken()
+    {
+
+        $uuid = Str::uuid()->toString();
+
+        $token = explode('-', $uuid)[0];
+        $captchaCode = substr(explode('-', $uuid)[4], 0, 6);
+
+        Cache::put($token, $captchaCode, 180); 
+>>>>>>> 1dfbcb5ad3453544b5e8f2bfddcf3da521cccb49
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    public function generate($token)
+    {
+
+        $captchaCode = Cache::get($token);
+
+        if (!$captchaCode) {
+            return response()->json([
+                'message' => 'Invalid or expired CAPTCHA token'
+            ], 404);
+        }
+
+        $image = imagecreate(120, 40);
+        $bgColor = imagecolorallocate($image, 255, 255, 255);
+        $textColor = imagecolorallocate($image, 0, 0, 255);
+
+        imagestring($image, 5, 10, 10, $captchaCode, $textColor);
+
+
+        ob_start();
+        imagepng($image);
+        $imageData = ob_get_contents();
+        ob_end_clean();
+
+
+        imagedestroy($image);
+
+        return response($imageData)->header('Content-Type', 'image/png');
+    }
+    public function verify(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'code' => 'required|string',
+            'token' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $captchaCode = Cache::get($request->token);
+        // dd($captchaCode);
+        if (!$captchaCode) {
+            return response()->json([
+                'message' => 'Invalid or expired CAPTCHA token'
+            ], 404);
+        }
+
+        if ($captchaCode === $request->input('code')) {
+
+            Cache::forget($request->token);
+
+            return response()->json([
+                'message' => 'CAPTCHA verified successfully'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'CAPTCHA verification failed'
+            ], 422);
+        }
+    }
+}
